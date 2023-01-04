b1a6316d0dacf487f7c4c80e693df6d965cee498
sujikim <sujikim@student.42seoul.kr>
2023-01-04 20:03:28 +0900

    FEAT: 기본 컴포넌트 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-function App() {
-  return <div className="App">App</div>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import WelcomePage from "./pages/WelcomePage";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
