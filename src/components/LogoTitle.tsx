352ed398a445e29351ac43afcab8b95cb6d034fe
sujikim <sujikim@student.42seoul.kr>
2023-01-04 21:51:01 +0900

    FEAT: 로고 타이틀 추가

---
+++ src/components/LogoTitle.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ReactComponent as Logo } from "../assets/image/logo.svg";
+import "../assets/css/LogoTitle.css";
+
+const LogoTitle = () => {
+  return (
+    <div className="logo-title__wrapper">
+      <Logo />
+      <h1>TO-DO</h1>
+    </div>
+  );
+};
+
+export default LogoTitle;
