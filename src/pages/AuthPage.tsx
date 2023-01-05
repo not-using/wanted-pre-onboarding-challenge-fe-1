b7018cdc10ea4b4a641e08cb9c404d3b9783dc9d
sujikim <sujikim@student.42seoul.kr>
2023-01-05 16:01:28 +0900

    FEAT: 토큰이 있으면 auth 페이지 접속시 리다이렉트

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import usePath from "../hooks/usePath";
 import LogoTitle from "../components/LogoTitle";
 import AuthForm from "../components/AuthForm";
 import LinkButton from "../components/LinkButton";
 import "../assets/css/AuthPage.css";
+import tokenContext from "../contexts/tokenContext";
+import { useNavigate } from "react-router-dom";
 
 const AuthPage = () => {
   const path = usePath();
+  const { token } = useContext(tokenContext);
+  const navigate = useNavigate();
   const isSignIn = path === "/signin";
+
+  useEffect(() => {
+    if (token !== null) navigate("/");
+  }, [navigate, token]);
 
   return (
     <main className="auth-page__wrapper">
