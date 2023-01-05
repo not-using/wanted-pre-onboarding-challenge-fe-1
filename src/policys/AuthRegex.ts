b273aad7aa52107921bd77f0d1f4ecc420894744
sujikim <sujikim@student.42seoul.kr>
2023-01-05 15:32:15 +0900

    FIX: 이메일 및 비밀번호 정규식 수정

--- src/policys/AuthRegex.ts
+++ src/policys/AuthRegex.ts
@@ -1,4 +1,4 @@
-export const emailRegex = /.+@.+\..+/; 
-// 이메일 최소조건 : @ .을 포함하고 앞뒤로 문자 및 숫자
-export const passwordRegex = /.{8,}/; 
+export const emailRegex = /[\d\w]*@[\d\w]+\.[\d\w]+/; 
+// 이메일 최소조건 :  @ .을 포함하고 앞뒤로 문자 및 숫자
+export const passwordRegex = /[\d\w!@#$%^&*()\-_=+{};:,<.>]{8,}/; 
 // 비밀번호 최소조건 : 8자 이상
