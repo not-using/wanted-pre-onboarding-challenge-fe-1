eda55c163642b70594f9e5eae66dac9244fe5191
sujikim <sujikim@student.42seoul.kr>
2023-01-05 20:36:58 +0900

    FEAT: textarea 컴포넌트 추가

---
+++ src/components/Textarea.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "../assets/css/Textarea.css";
+
+interface textareaProps {
+  className?: string;
+  placeholder?: string;
+  value: string;
+  labelText?: string;
+  minLength?: number;
+  maxLength?: number;
+  pattern?: string;
+  onChange: (value: string) => void;
+  patternErrorMessage?: string;
+  autoComplete?: string;
+  required?: boolean;
+  validateRequired?: boolean;
+  isValid?: boolean;
+}
+const Textarea = ({
+  className,
+  placeholder,
+  minLength,
+  maxLength,
+  value,
+  onChange,
+  required = false,
+}: textareaProps) => {
+  const onChangeTextarea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = e.currentTarget;
+    onChange(value);
+  };
+  return (
+    <textarea
+      className={`textarea__wrapper ${className ?? ""}`}
+      value={value}
+      onChange={onChangeTextarea}
+      placeholder={placeholder}
+      required={required}
+      minLength={minLength}
+      maxLength={maxLength}
+    ></textarea>
+  );
+};
+
+export default Textarea;
