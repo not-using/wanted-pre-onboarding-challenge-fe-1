647973576825d3d94b97621e8390a7ce403903a5
sujikim <sujikim@student.42seoul.kr>
2023-01-13 04:01:21 +0900

    FIX: url 탐지 및 컴포넌트 분기를 route형식으로 대체

--- src/components/auth/index.tsx
+++ src/components/auth/index.tsx
@@ -1,25 +1,18 @@
-import { usePath } from "hooks/.commons/usePath";
-import AuthForm from "components/auth/AuthForm";
+import { Route, Routes } from "react-router-dom";
+import Login from "components/auth/Login";
+import SignUp from "components/auth/SignUp";
 import LogoTitle from "components/.commons/LogoTitle";
-import LinkButton from "components/.commons/LinkButton";
 import "assets/css/AuthPage.css";
 
 const AuthPage = () => {
-  const path = usePath();
-  const isSignIn = path === "/auth";
-  
   return (
     <main className="auth-page__wrapper">
       <LogoTitle />
       <section className="auth-page__contents">
-        <AuthForm isSignIn={isSignIn} />
-        {isSignIn ? (
-          <LinkButton
-            className="auth-page__signup-button"
-            path="/signup"
-            value="회원가입"
-          />
-        ) : null}
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
       </section>
     </main>
   );
--- src/components/todo/index.tsx
+++ src/components/todo/index.tsx
@@ -1,22 +1,25 @@
 import { Suspense } from "react";
-import { usePath } from "hooks/.commons/usePath";
+import { Route, Routes } from "react-router-dom";
+import TodoWelcome from "components/todo/TodoWelcome";
 import TodoForm from "components/todo/TodoForm";
 import TodoList from "components/todo/TodoList";
 import TodoDetail from "components/todo/TodoDetail";
-import Loading from "../.commons/Loading";
+import Loading from "components/.commons/Loading";
 import "assets/css/TodoPage.css";
 
 const TodoPage = () => {
-  const path = usePath();
-
   return (
     <main className="todo-page__wrapper">
       <Suspense fallback={<Loading />}>
         <TodoList />
       </Suspense>
-      <Suspense fallback={<Loading />}>
-        {path === "/" || path === "/create" ? <TodoForm /> : <TodoDetail />}
-      </Suspense>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<TodoWelcome />} />
+            <Route path="/create" element={<TodoForm />} />
+            <Route path="*" element={<TodoDetail />} />
+          </Routes>
+        </Suspense>
     </main>
   );
 };
