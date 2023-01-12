627d4eb77f68be43568f99738acb45372bc135fc
sujikim <sujikim@student.42seoul.kr>
2023-01-12 20:49:16 +0900

    FEAT: todo page 로딩 중을 표현하기 위한 Suspense 추가

--- src/components/todo/index.tsx
+++ src/components/todo/index.tsx
@@ -1,8 +1,10 @@
+import { Suspense } from "react";
 import { useGetTodos } from "hooks/todo/useGetTodos";
 import { usePath } from "hooks/.commons/usePath";
 import TodoForm from "components/todo/TodoForm";
 import TodoList from "components/todo/TodoList";
 import TodoDetail from "components/todo/TodoDetail";
+import Loading from "../.commons/Loading";
 import "assets/css/TodoPage.css";
 
 const TodoPage = () => {
@@ -11,12 +13,14 @@ const TodoPage = () => {
 
   return (
     <main className="todo-page__wrapper">
-      <TodoList todoList={todoList} />
-      {path === "/" || path === "/create" ? (
-        <TodoForm addTodo={addTodo} />
-      ) : (
-        <TodoDetail updateTodo={updateTodo} removeTodo={removeTodo} />
-      )}
+      <Suspense fallback={<Loading />}>
+        <TodoList todoList={todoList} />
+        {path === "/" || path === "/create" ? (
+          <TodoForm addTodo={addTodo} />
+        ) : (
+          <TodoDetail updateTodo={updateTodo} removeTodo={removeTodo} />
+        )}
+      </Suspense>
     </main>
   );
 };
